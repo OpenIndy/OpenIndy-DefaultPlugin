055b78961869eb2f2c34ebf2021211b1d0f42ae5
Schulte, Eberhard <schulte@sigma3d.de>
2020-03-17 08:21:46 +0100

    OI-520: initial test

--- test/function/tst_function.cpp
+++ test/function/tst_function.cpp
@@ -366,6 +366,7 @@ public:
     FunctionTest();
 
 private Q_SLOTS:
+    void testDistance_PointFromPoints_Register();
     void testPointFromPoints_Register2();
     void testPointFromPoints_Register();
 
@@ -3305,6 +3306,33 @@ void FunctionTest::testPointFromPoints_Register2() {
     COMPARE_DOUBLE(feature->getPosition().getVector().getAt(0), (  710.79084819), 0.0001);
     COMPARE_DOUBLE(feature->getPosition().getVector().getAt(1), ( 2281.722941), 0.0001);
     COMPARE_DOUBLE(feature->getPosition().getVector().getAt(2), (  153.1470537), 0.0001);
+
+}
+
+void FunctionTest::testDistance_PointFromPoints_Register() {
+    ChooseLALib::setLinearAlgebra(ChooseLALib::Armadillo);
+
+    QPointer<ScalarEntityDistance> feature = new ScalarEntityDistance(false);
+    QPointer<FeatureWrapper> wrapper = new FeatureWrapper();
+    wrapper->setScalarEntityDistance(feature);
+
+    QPointer<Function> function = createFunction("DistanceFromPlane");
+    QVERIFY2(!function.isNull(), "function is null");
+
+    // colum delim: " "
+    // line ending: "\n"
+    // unit:        [mm]
+    addInputCircle(1000.6609, 2000.3247, 3000.3180, 0.0, 0.0, 1.0, 1.0, function, 2000, 0);
+
+    addInputPlane(0., 0., 0., 0.10080018, -0.09785417,  0.99008277, function, 2000, 1);
+
+    feature->addFunction(function);
+    QVERIFY2(feature->getDisplayFunctions().compare("DistanceFromPlane")==0, "getDisplayFunctions");
+
+    feature->recalc();
+    QVERIFY2(feature->getIsSolved(), "recalc");
+
+    COMPARE_DOUBLE(feature->getDistance(), 1881.5050, 0.0001);
 
 }
 QTEST_APPLESS_MAIN(FunctionTest)
