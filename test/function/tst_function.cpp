e889392a46d4efa01624a6a47367db50e1b405b4
Schulte, Eberhard <schulte@sigma3d.de>
2020-03-17 11:18:06 +0100

    OI-520: refactoring

--- test/function/tst_function.cpp
+++ test/function/tst_function.cpp
@@ -90,6 +90,22 @@ struct ConfiguredFunctionConfig {
         }
     }
 
+    QList<NeededElement> getNeededElements() {
+        QList<NeededElement> list;
+        for(int i=0; i<this->neededElements.size(); i++) {
+            list.append(this->neededElements[i].second);
+        }
+        return list;
+    }
+
+    QList<QString> getNeededElementNames() {
+        QList<QString> list;
+        for(int i=0; i<this->neededElements.size(); i++) {
+            list.append(this->neededElements[i].first);
+        }
+        return list;
+    }
+
 };
 
 class DistanceBetweenTwoPoints : public ConstructFunction
