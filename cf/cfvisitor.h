1a7a15f87d69093c1e123fa91e33dcf6cb2a70a9
Schulte, Eberhard <schulte@sigma3d.de>
2021-02-02 14:36:13 +0100

    OI-520: clean up: move version 2 to default and only existing

---
+++ cf/cfvisitor.h
@@ -0,0 +1,24 @@
+#ifndef CFVISITOR_H
+#define CFVISITOR_H
+
+#include "treeutil.h"
+#include "configuredfunctioncommon.h"
+#include "configuredfunction.h"
+#include "cfcparameter.h"
+
+using namespace oi;
+
+class CFVisitor: public NodeVisitor {
+
+public:
+    CFVisitor(CFContext ctx);
+
+    void pre(QPointer<Node> node, int index, int level) override;
+    void post(QPointer<Node> node, int index, int level) override;
+
+private:
+    CFContext ctx;
+    CFData data;
+};
+
+#endif // CFVISITOR_H
