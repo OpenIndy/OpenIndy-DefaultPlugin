30c86b4cb0df676dd89f68ce8b7ff9e75e29fd96
Schulte, Eberhard <schulte@sigma3d.de>
2021-02-02 13:58:00 +0100

    OI-660: XYZFilter: filter axis

---
+++ cf/xyzfilter.h
@@ -0,0 +1,28 @@
+#ifndef XYZFILTER_H
+#define XYZFILTER_H
+#include <QObject>
+
+#include "constructfunction.h"
+#include "featurewrapper.h"
+#include "oivec.h"
+
+using namespace oi;
+
+class XYZFilter : public ConstructFunction
+{
+    Q_OBJECT
+public:
+    XYZFilter(QString name, bool removeX, bool removeY, bool removeZ, QObject *parent = 0);
+
+    void init() override;
+
+    bool exec(Point &point) override;
+
+private:
+
+    bool removeX;
+    bool removeY;
+    bool removeZ;
+};
+
+#endif // XYZFILTER_H
