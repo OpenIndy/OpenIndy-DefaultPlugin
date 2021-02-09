e8b643f97a59850c472a903787ac820564eb6d99
Schulte, Eberhard <schulte@sigma3d.de>
2021-02-09 17:13:18 +0100

    OI-520: comment

--- cf/configuredfunction.cpp
+++ cf/configuredfunction.cpp
@@ -5,7 +5,7 @@ ConfiguredFunction::ConfiguredFunction(ConfiguredFunctionConfig config, QList<QP
 
 void ConfiguredFunction::init() {
     this->metaData.name = this->config.name;
-    this->metaData.iid = SpecialFunction_iidd;
+    this->metaData.iid = SpecialFunction_iidd; // or differentiate?
 
     this->applicableFor = this->config.applicableFor;
     this->neededElements = config.getNeededElements();
