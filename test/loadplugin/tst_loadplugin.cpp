3fe7f69107b2eba53e2d45830bfa96443160f383
Schulte, Eberhard <schulte@sigma3d.de>
2022-11-09 08:45:14 +0100

    OI-945: test load plugin

---
+++ test/loadplugin/tst_loadplugin.cpp
@@ -0,0 +1,59 @@
+#include <QString>
+#include <QtTest>
+#include <QPointer>
+#include <QList>
+#include "plugin.h"
+
+using namespace oi;
+
+class LoadPluginTest : public QObject
+{
+    Q_OBJECT
+
+public:
+    LoadPluginTest();
+
+private Q_SLOTS:
+    void testLoadPlugin();
+
+private:
+
+};
+
+LoadPluginTest::LoadPluginTest()
+{
+}
+
+/*
+ * PATH:
+ * OpenIndy-DefaultPlugin\lib\OpenIndy-Core\bin\debug\;
+ * OpenIndy-DefaultPlugin\lib\OpenIndy-Core\lib\OpenIndy-Math\bin\debug\;
+ * OpenIndy-DefaultPlugin\lib\OpenIndy-Core\lib\OpenIndy-Math\lib\armadillo-3.910.0\examples\lib_win64\
+ *
+ * Cannot load library ... Das angegebene Modul wurde nicht gefunden.
+ * -> dll kann nicht geladen werden, weil z. B. benötigte dll nicht im Pfad ist
+ *
+ * Cannot load library ... Die angegebene Prozedur wurde nicht gefunden.
+ * -> ???
+ */
+void LoadPluginTest::testLoadPlugin(){
+
+    QDir dir;
+    QString pluginPath = dir.absoluteFilePath("../../bin/debug/p_defaultPlugind1.dll");
+    QVERIFY2(QFile::exists(pluginPath), pluginPath.toLatin1().data());
+    QPluginLoader pluginLoader(pluginPath);
+
+    QObject *plugin = pluginLoader.instance();
+    QVERIFY2(plugin, pluginLoader.errorString().toLatin1().data());
+
+    Plugin *oiPlugin = qobject_cast<Plugin *>(plugin);
+    QVERIFY2(oiPlugin, "qobject_cast");
+
+    QList<QPointer<Function> > functions = oiPlugin->createFunctions();
+    QVERIFY2(functions.size() > 0, "no functions found");
+
+}
+
+QTEST_APPLESS_MAIN(LoadPluginTest)
+
+#include "tst_loadplugin.moc"
